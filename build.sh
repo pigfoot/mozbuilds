3db0c9986d0cc07162dad5077981700490793a27
Chih-Chia Chen <pigfoot@gmail.com>
2011-08-16 13:35:52 +0800

    Adjust build sequence from P1,P2,P3 to P3,P2,P1.

--- build.sh
+++ build.sh
@@ -10,53 +10,53 @@ BUILD_ID="FF5"
 MozBuilder=$MOZBUILDPFPATCHDIR/toolkit/MozBuilder.py
 
 ##############################################################################################
-$MozBuilder fx ${BUILD_ID} p1 en_US no-branding release public
-$MozBuilder fx ${BUILD_ID} p2 en_US no-branding release public
 $MozBuilder fx ${BUILD_ID} p3 en_US no-branding release public
+$MozBuilder fx ${BUILD_ID} p2 en_US no-branding release public
+$MozBuilder fx ${BUILD_ID} p1 en_US no-branding release public
 
 ##############################################################################################
-$MozBuilder fx ${BUILD_ID} p1 zh_TW no-branding release public
-$MozBuilder fx ${BUILD_ID} p2 zh_TW no-branding release public
 $MozBuilder fx ${BUILD_ID} p3 zh_TW no-branding release public
+$MozBuilder fx ${BUILD_ID} p2 zh_TW no-branding release public
+$MozBuilder fx ${BUILD_ID} p1 zh_TW no-branding release public
 
 ##############################################################################################
-$MozBuilder fx ${BUILD_ID} p1 zh_CN no-branding release public
-$MozBuilder fx ${BUILD_ID} p2 zh_CN no-branding release public
 $MozBuilder fx ${BUILD_ID} p3 zh_CN no-branding release public
+$MozBuilder fx ${BUILD_ID} p2 zh_CN no-branding release public
+$MozBuilder fx ${BUILD_ID} p1 zh_CN no-branding release public
 
 ##############################################################################################
-$MozBuilder fx ${BUILD_ID} p1 ja no-branding release public
-$MozBuilder fx ${BUILD_ID} p2 ja no-branding release public
 $MozBuilder fx ${BUILD_ID} p3 ja no-branding release public
+$MozBuilder fx ${BUILD_ID} p2 ja no-branding release public
+$MozBuilder fx ${BUILD_ID} p1 ja no-branding release public
 
 ##############################################################################################
-$MozBuilder fx ${BUILD_ID} p1 de no-branding release public
-$MozBuilder fx ${BUILD_ID} p2 de no-branding release public
 $MozBuilder fx ${BUILD_ID} p3 de no-branding release public
+$MozBuilder fx ${BUILD_ID} p2 de no-branding release public
+$MozBuilder fx ${BUILD_ID} p1 de no-branding release public
 
 ##############################################################################################
-$MozBuilder fx ${BUILD_ID} p1 fr no-branding release public
-$MozBuilder fx ${BUILD_ID} p2 fr no-branding release public
 $MozBuilder fx ${BUILD_ID} p3 fr no-branding release public
+$MozBuilder fx ${BUILD_ID} p2 fr no-branding release public
+$MozBuilder fx ${BUILD_ID} p1 fr no-branding release public
 
 ##############################################################################################
-$MozBuilder fx ${BUILD_ID} p1 it no-branding release public
-$MozBuilder fx ${BUILD_ID} p2 it no-branding release public
 $MozBuilder fx ${BUILD_ID} p3 it no-branding release public
+$MozBuilder fx ${BUILD_ID} p2 it no-branding release public
+$MozBuilder fx ${BUILD_ID} p1 it no-branding release public
 
 ##############################################################################################
-$MozBuilder fx ${BUILD_ID} p1 ru no-branding release public
-$MozBuilder fx ${BUILD_ID} p2 ru no-branding release public
 $MozBuilder fx ${BUILD_ID} p3 ru no-branding release public
+$MozBuilder fx ${BUILD_ID} p2 ru no-branding release public
+$MozBuilder fx ${BUILD_ID} p1 ru no-branding release public
 
 ##############################################################################################
-$MozBuilder fx ${BUILD_ID} p1 sl no-branding release public
-$MozBuilder fx ${BUILD_ID} p2 sl no-branding release public
 $MozBuilder fx ${BUILD_ID} p3 sl no-branding release public
+$MozBuilder fx ${BUILD_ID} p2 sl no-branding release public
+$MozBuilder fx ${BUILD_ID} p1 sl no-branding release public
 
 ##############################################################################################
-$MozBuilder fx ${BUILD_ID} p1 tr no-branding release public
-$MozBuilder fx ${BUILD_ID} p2 tr no-branding release public
 $MozBuilder fx ${BUILD_ID} p3 tr no-branding release public
+$MozBuilder fx ${BUILD_ID} p2 tr no-branding release public
+$MozBuilder fx ${BUILD_ID} p1 tr no-branding release public
 
 cd ..
